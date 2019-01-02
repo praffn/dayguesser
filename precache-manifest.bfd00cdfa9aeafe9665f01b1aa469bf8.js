4fc3cf36d49d20e694b1f4f465a4ed2ca148905f
Phillip Raffnsøe <phillip@praffn.dk>
2019-01-02 02:31:12 +0100

    Updates

---
+++ precache-manifest.bfd00cdfa9aeafe9665f01b1aa469bf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8943f37e525e0971f86a",
+    "url": "/dayguesser/static/js/runtime~main.8943f37e.js"
+  },
+  {
+    "revision": "67f0f735ae1f6385e9e8",
+    "url": "/dayguesser/static/js/main.67f0f735.chunk.js"
+  },
+  {
+    "revision": "25388bcdc967629ee488",
+    "url": "/dayguesser/static/js/1.25388bcd.chunk.js"
+  },
+  {
+    "revision": "67f0f735ae1f6385e9e8",
+    "url": "/dayguesser/static/css/main.5d4bf1f6.chunk.css"
+  },
+  {
+    "revision": "62d5f7125ce9f5c1f52e2d439a5e9041",
+    "url": "/dayguesser/index.html"
+  }
+];
